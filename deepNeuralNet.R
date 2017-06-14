c6b9c8b25bf4a39730fcf1fe5dec3d7aa21b3216
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-15 01:26:42 +0530

    Made the First MLP model on MNIST dataset

--- deepNeuralNet.R
+++ deepNeuralNet.R
@@ -33,3 +33,55 @@ install_tensorflow()
 #For installing a gpu version of Tensorflow
 install_tensorflow(gpu = T)
 
+
+#Getting started with Keras for R
+#The core data structure of Keras is a model, a way to organize layers. 
+#The simplest type of model is the Sequential model, a linear stack of layers. 
+#For more complex architectures, you should use the Keras functional API, 
+#which allows to build arbitrary graphs of layers.
+
+#loading the keras inbuilt mnist dataset
+data<-dataset_mnist()
+?dataset_mnist #MNIST database of handwritten digits
+
+
+#Training Data
+train_x<-data$train$x
+train_y<-data$train$y
+
+#Test Set
+test_x<-data$test$x
+test_y<-data$test$y
+
+#converting a 2D array into a 1D array for feeding 
+#into the MLP and normalising the matrix
+train_x<-array(train_x,dim=c(dim(train_x)[1],prod(dim(train_x)[-1])))/255
+test_x<-array(train_x,dim=c(dim(train_x)[1],prod(dim(train_x)[-1])))/255
+
+
+#converting the target variable to once hot encoded 
+#vectors using keras inbuilt function
+
+train_y<-to_categorical(train_y,10)
+test_y<-to_categorical(test_y,10)
+
+#Now defining a keras MLP sequential model
+model <- keras_model_sequential()
+
+#defining the model with 1 input layer[784 neurons], 1 hidden layer[784 neurons] 
+#with dropout rate 0.4 and 1 output layer[10 neurons]
+#i.e number of digits from 0 to 9
+
+model %>% 
+  #Add a densely-connected NN layer to an output
+  layer_dense(units=784,input_shape=784)  %>%
+  #dropout layer to prevent Overfitting
+  layer_dropout(rate=0.4) %>%
+  #Apply an activation function to an output.
+  #Relu can only be used for Hidden layers
+  layer_activation(activation="relu") %>%
+  layer_dense(units=10) %>%
+  #Apply an activation function to an output layer
+  #softmax activation for Output layer which computes the probabilities for the classes
+  layer_activation(activation="softmax") 
+
